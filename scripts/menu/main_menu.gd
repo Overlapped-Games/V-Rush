8fb8c6889017015f8713fe733bbc265f8309a1db
loremIpsum6321 <125786603+loremIpsum6321@users.noreply.github.com>
2024-01-23 17:35:23 -0800

    sound on main menu
    sound

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -4,9 +4,7 @@ var fade_timer : Timer
 var fade_duration : float = 1
 var fade = false
 
-func _ready():
-	#$Control/VBoxContainer/Play.grab_focus()
-	# Create and connect the Timer
+func _ready() -> void:
 	fade_timer = Timer.new()
 	add_child(fade_timer)
 	connect("fade_timeout", play_menu_transition)
@@ -21,32 +19,29 @@ func fade_out(delta) -> void:
 	else:
 		play_menu_transition()
 
-func _on_play_pressed():
+func _on_play_pressed() -> void:
 	fade = true
 
 func play_menu_transition() -> void:
 	get_tree().change_scene_to_file("res://assets/menus/scenes/transition.tscn")
-
+	
 func _on_settings_pressed() -> void:
-	print('settings')
 	get_tree().change_scene_to_file("res://assets/menus/scenes/settings.tscn")
-
-func _on_quit_pressed():
+	
+func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
-
 # --------- mouse hover event ------------
-func _on_play_shader_mouse_entered():
+func _on_play_shader_mouse_entered() -> void:
 	$Control/PlayShader.visible = false
-func _on_play_shader_mouse_exited():
+func _on_play_shader_mouse_exited() -> void:
 	$Control/PlayShader.visible = true
-func _on_settings_shader_mouse_entered():
+func _on_settings_shader_mouse_entered() -> void:
 	$Control/SettingsShader.visible = false
-func _on_settings_shader_mouse_exited():
+func _on_settings_shader_mouse_exited() -> void:
 	$Control/SettingsShader.visible = true
-func _on_quit_shader_mouse_entered():
+func _on_quit_shader_mouse_entered() -> void:
 	$Control/QuitShader.visible = false
-func _on_quit_shader_mouse_exited():
+func _on_quit_shader_mouse_exited() -> void:
 	$Control/QuitShader.visible = true
 # ----------------------------------------
